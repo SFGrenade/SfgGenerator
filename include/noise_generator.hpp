b3e1f2f87fd11b4f704f7613488d924dbdbd4d0f
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2026-01-31 22:23:57 +0100

    have upow/ipow always be 64bit

--- include/noise_generator.hpp
+++ include/noise_generator.hpp
@@ -119,22 +119,20 @@ class NoiseGenerator : BasePlugin {
 }
 #endif
 
-template < class Int1, class Int2 >
-uintmax_t sfg_upow( Int1 a, Int2 b ) {
+uint64_t sfg_upow( uint64_t a, uint64_t b ) {
   b = b - 1;
   if( b <= -2 )
     throw std::range_error( "negatives not yet handled" );
   if( b >= 0 )
     return a << b;
-  return a >> std::abs< Int2 >( b );
+  return a >> std::abs< uint64_t >( b );
 }
 
-template < class Int1, class Int2 >
-intmax_t sfg_ipow( Int1 a, Int2 b ) {
+int64_t sfg_ipow( int64_t a, int64_t b ) {
   b = b - 1;
   if( b <= -2 )
     throw std::range_error( "negatives not yet handled" );
   if( b >= 0 )
     return a << b;
-  return a >> std::abs< Int2 >( b );
+  return a >> std::abs< int64_t >( b );
 }
