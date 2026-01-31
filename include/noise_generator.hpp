57c6b110f40e19dd4a9c1eb13b4a32201ad76ddb
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2026-01-31 22:38:26 +0100

    declare/define in header/source

--- include/noise_generator.hpp
+++ include/noise_generator.hpp
@@ -119,20 +119,5 @@ class NoiseGenerator : BasePlugin {
 }
 #endif
 
-uint64_t sfg_upow( uint64_t a, uint64_t b ) {
-  b = b - 1;
-  if( b <= -2 )
-    throw std::range_error( "negatives not yet handled" );
-  if( b >= 0 )
-    return a << b;
-  return a >> std::abs< uint64_t >( b );
-}
-
-int64_t sfg_ipow( int64_t a, int64_t b ) {
-  b = b - 1;
-  if( b <= -2 )
-    throw std::range_error( "negatives not yet handled" );
-  if( b >= 0 )
-    return a << b;
-  return a >> std::abs< int64_t >( b );
-}
+uint64_t sfg_upow( uint64_t a, uint64_t b );
+int64_t sfg_ipow( int64_t a, int64_t b );
--- src/noise_generator.cpp
+++ src/noise_generator.cpp
@@ -1560,3 +1560,21 @@ clap_plugin_descriptor_t* NoiseGenerator::descriptor_get( void ) {
 }
 
 #pragma endregion
+
+uint64_t sfg_upow( uint64_t a, uint64_t b ) {
+  b = b - 1;
+  if( b <= -2 )
+    throw std::range_error( "negatives not yet handled" );
+  if( b >= 0 )
+    return a << b;
+  return a >> std::abs< uint64_t >( b );
+}
+
+int64_t sfg_ipow( int64_t a, int64_t b ) {
+  b = b - 1;
+  if( b <= -2 )
+    throw std::range_error( "negatives not yet handled" );
+  if( b >= 0 )
+    return a << b;
+  return a >> std::abs< int64_t >( b );
+}
